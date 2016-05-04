9160b6ef5e21b61e9375bf20d0cd611b73b959b7
itsRichard <richardtlee@gmail.com>
2016-05-03 17:35:35 -0700

    Creation

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public float autoLoadNextLevelAfter;
+
+    void Start()
+    {
+        Debug.Log("Loaded LoadManager");
+        Invoke("LoadLevelNext", autoLoadNextLevelAfter);
+    }
+
+	public void LoadLevel(string name){
+		Debug.Log ("New Level load: " + name);
+        // Application.LoadLevel (name);    Deprecated for Unity 5
+        SceneManager.LoadScene(name);
+	}
+
+    public void LoadLevelNext()
+    {
+        Debug.Log("New Level load" + name);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+	public void QuitRequest(){
+		Debug.Log ("Quit requested");
+        // Application.Quit ();
+        Application.Quit();
+	}
+
+}
