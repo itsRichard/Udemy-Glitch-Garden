f2a58dcecb8d19189dce1a4f9587000708f0d017
itsRichard <rtl@uw.edu>
2016-05-05 10:41:28 -0700

    More splash screen updates

---
+++ Assets/Scripts/Splash.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Splash : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log(SceneManager.GetActiveScene().buildIndex);
+    }
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
