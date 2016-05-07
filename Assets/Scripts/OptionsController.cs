2efa53dc988beb481fc28213041fca736bc1425e
itsRichard <rtl@uw.edu>
2016-05-07 07:13:17 -0700

    players pref 2

---
+++ Assets/Scripts/OptionsController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class OptionsController : MonoBehaviour {
+
+	public Slider volumeSlider;
+	public LevelManager levelmanager;
+	public MusicManager musicManager;
+
+	// Use this for initialization
+	void Start () {
+
+		musicManager = GameObject.FindObjectOfType<MusicManager> ();
+		Debug.Log (musicManager);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
