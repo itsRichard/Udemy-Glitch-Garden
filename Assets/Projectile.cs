9a1c3ed52167496eeb4c44bfa0649e322bed0758
itsRichard <richardtlee@gmail.com>
2016-05-11 19:17:25 -0700

    animation

--- Assets/Projectile.cs
+++ Assets/Projectile.cs
@@ -14,4 +14,18 @@ public class Projectile : MonoBehaviour {
 	void Update () {
 		transform.Translate (Vector3.right * speed * Time.deltaTime);
 	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Attacker attacker = col.gameObject.GetComponent<Attacker>();
+        Health health = col.gameObject.GetComponent<Health>();
+
+        if (attacker && health)
+        {
+            health.DealDamage(damage);
+            Destroy(gameObject);
+        }
+
+
+    }
 }
---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooter : MonoBehaviour {
+
+    public GameObject projectile, projectileParent,gun;
+
+    private void Fire()
+    {
+        GameObject newProjectile = Instantiate(projectile) as GameObject;    //create a new project
+        newProjectile.transform.parent = projectileParent.transform;    //create the projectile att he position of parent
+        newProjectile.transform.position = gun.transform.position;
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
