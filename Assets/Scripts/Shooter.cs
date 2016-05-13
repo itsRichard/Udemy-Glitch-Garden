04dcb22b1ff895525ac2d5e3c0059274edf2eb4e
itsRichard <itsRichard@users.noreply.github.com>
2016-05-13 11:26:17 -0700

    Update Shooter.cs
    Added moments

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -3,13 +3,17 @@ using System.Collections;
 
 public class Shooter : MonoBehaviour {
 
+    // What's being shot, who is it being shot by, and what's it being shot out of.
+    // All different elements of the defender e.g. A cactus shoots courgettes out of its own gun 
     public GameObject projectile, projectileParent,gun;
 
+    // Fire is trigged by the defender animation event
     private void Fire()
     {
-        GameObject newProjectile = Instantiate(projectile) as GameObject;    //create a new project
-        newProjectile.transform.parent = projectileParent.transform;    //create the projectile att he position of parent
-        newProjectile.transform.position = gun.transform.position;
+        GameObject newProjectile = Instantiate(projectile) as GameObject;    //create a new object (projectile)
+        newProjectile.transform.parent = projectileParent.transform;    //assign the object as a child of its parent (the shooter)
+        newProjectile.transform.position = gun.transform.position;  // place the projectile at the position of the gun 
+        // further movement of the projectile is dicated by projectile.cs.  
     }
 
     void OnBecameInvisible()
