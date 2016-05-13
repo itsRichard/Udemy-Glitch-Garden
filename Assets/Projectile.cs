735b628ac3484863eb16980065106ae5e85470cc
itsRichard <itsRichard@users.noreply.github.com>
2016-05-13 11:20:14 -0700

    Update Projectile.cs

--- Assets/Projectile.cs
+++ Assets/Projectile.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Projectile : MonoBehaviour {
 
 	public float speed, damage;
+	// These are set by the prefab gameobject which this script is attached to.  
 
 	// Use this for initialization
 	void Start () {
@@ -13,7 +14,9 @@ public class Projectile : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		transform.Translate (Vector3.right * speed * Time.deltaTime);
+		// As soon as this thing is created, move it right.  
 	}
+
 
     void OnTriggerEnter2D(Collider2D col)
     {
