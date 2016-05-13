7ca94350f0b670a7633cd25ae11eb3e9b37f0af8
itsRichard <richardtlee@gmail.com>
2016-05-12 18:54:33 -0700

    Buttons

---
+++ Assets/Button.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+
+    public GameObject defenderPrefab;
+
+    private Button[] buttonArray;
+    public static GameObject selectedDefender;     //static means there is just one of them in the entire game.  on the mold.  
+
+	// Use this for initialization
+	void Start () {
+        buttonArray = GameObject.FindObjectsOfType<Button>();
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseDown()
+    {
+        foreach (Button thisButton in buttonArray)
+        {
+            thisButton.GetComponent<SpriteRenderer>().color = Color.black;
+        }
+        GetComponent<SpriteRenderer>().color = Color.white;
+        selectedDefender = defenderPrefab;
+        print(selectedDefender);
+
+    }
+}
